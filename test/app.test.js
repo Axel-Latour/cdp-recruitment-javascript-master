565782a8ccd507bb2bf514a8af299dcc31d3efe1
Axel Latour <axel.latour3@gmail.com>
2021-04-29 15:05:59 +0200

    Fix test name

--- test/app.test.js
+++ test/app.test.js
@@ -6,7 +6,7 @@ const { executeCommandFromArgs } = require('../services/commandService');
 
 describe('app starts', () => {
   process.argv = ['node', 'app.js', '--count'];
-  it('should call main method with command options', () => {
+  it('should call executeCommandFromArgs method with command options', () => {
     const executeCommandFromArgsMock = jest.fn();
 
     executeCommandFromArgs.mockImplementation(executeCommandFromArgsMock);
